969e2e52bd9d734502e351cc34e612f692756b90
Tim LaRou <larou.tim@gmail.com>
2017-03-09 21:40:25 -0600

    switch to color theif

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -71,30 +71,61 @@ function createPokemon() {
     img2.crossOrigin = "Anonymous";
 
     img2.addEventListener('load', function() {
-    
-        var vibrant = new Vibrant(img2);
-        var swatches = vibrant.swatches()
-        for (var swatch in swatches) {
-            if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
-                console.log(swatch, swatches[swatch].getHex())
-                var colorBox = $("<p>");
-                var pBox = $("<p>");
-                colorBox.attr("class", "colorBox")
-                var hexCode = swatches[swatch].getHex();
-                colorBox.css("background-color", hexCode)
-                pBox.text( hexCode);
-                midColRow.append(pBox);
-                leftColRow.append(colorBox);
+      var colorThief = new ColorThief();
+      var domColor = colorThief.getColor(img2);
+      var palette = colorThief.getPalette(img2);
+      var domColorBox = $("<p>");
+      var domPBox = $("<p>");
+      var rbgCode = "rgb(" + domColor[0] + "," + domColor[1] + "," + domColor[2]+")" ;
 
-                // if (swatch == "Vibrant") {
-                //  pokemonSpot.css("background-color",hexCode);
-                // }
-                //  if (swatch == "LightVibrant") {
-                //  midColRow.css("color",hexCode);
-                // }
-            }
-        }
+      domColorBox.attr("class", "colorBox");
+      domColorBox.css("background-color", rbgCode  );
+      domPBox.text = rbgCode;
+      midColRow.append(domPBox);
+      leftColRow.append(domColorBox);
+
+      for (var i = 0; i < palette.length; i ++) {
+        var colorBox = $("<p>");
+        var pBox = $("<p>");
+        rbgCode = "rgb(" + palette[i][0] + "," + palette[i][1] + "," + palette[i][2]+")";
+        pBox.text( rbgCode);
+        midColRow.append(pBox);
+
+    //             midColRow.append(pBox);
+        colorBox.attr("class", "colorBox");
+        colorBox.css("background-color",rbgCode);
+        leftColRow.append(colorBox);
+      }
+
+      console.log(colorThief.getColor(img2));
+      console.log(colorThief.getPalette(img2));
+    //     var vibrant = new Vibrant(img2);
+    //     var swatches = vibrant.swatches()
+    //     for (var swatch in swatches) {
+    //         if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
+    //             console.log(swatch, swatches[swatch].getHex(),swatches[swatch].getPopulation() )
+    //             var colorBox = $("<p>");
+    //             var pBox = $("<p>");
+    //             colorBox.attr("class", "colorBox")
+    //             var hexCode = swatches[swatch].getHex();
+    //             colorBox.css("background-color", hexCode)
+    //             pBox.text( hexCode);
+    //             midColRow.append(pBox);
+    //             leftColRow.append(colorBox);
+
+    //             // if (swatch == "Vibrant") {
+    //             //  pokemonSpot.css("background-color",hexCode);
+    //             // }
+    //             //  if (swatch == "LightVibrant") {
+    //             //  midColRow.css("color",hexCode);
+    //             // }
+    //         }
+    //     }
     });
+
+
+
+    
 
     rightCol.append(pokemonImage);
     leftCol.append(leftColRow);
