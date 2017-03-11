a52388bdaffebfe7d6c5fc774249244e8936ba51
Michael Nguyen <nguyen.michl@gmail.com>
2017-03-11 00:45:35 -0600

    Last commit. To update: extensibility for selectors and css vars

--- drag-and-drop/drag-drop.js
+++ drag-and-drop/drag-drop.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
 	// Initialize a hard coded array of colors
-	var colors = ["#aceace", "#4f14f1", "#5d25c2", "#d4ad4a", "#eaeb12"];
+	var colors = ["#1D3E8C", "#413C58", "#D9F9A5", "#FFEBD6", "#A3C4BC"];
 
-	// Makes color-holder a drop acceptor
-	// $(".color-holder").droppable({
-	// 	drop: dropHandler
-	// });
+	// Initialize a hard coded object of CSS Variable "human" name and Logical name
+	// var cssVariableObject = {
+	// 	"Paragraph Text": "--para"
+	// }
 
 	// Generate the color to DOM elements and append to palette-box
+	// Function accepts an array of colors.
 	function generatePaletteDOM(colorsArray) {
+		// If relevant, clears container.
 		$(".palette-box").empty();
 
 		for (var i = 0; i < colorsArray.length; i++) {
 			var outputColor = $("<div>");
+			// Adds class for later use and attributes for storing color value.
 			outputColor.addClass("palette-color");
 			outputColor.attr("data-color-value", colorsArray[i]);
 			outputColor.text(colorsArray[i]);
@@ -24,27 +27,34 @@ $(document).ready(function(){
 				helper: "clone",
 				revert: true
 			});
+
 			$(".palette-box").append(outputColor);
-		}
-		
+		}	
 	}
 
-	// function dropHandler(event, ui){
-	// 	console.log(ui);
-	// }
+	function generateStyleContainer(styleObject){
 
+	}
+
+	// Adds drop functionality to DOM element.
 	$(".color-holder").droppable();
+
+	// Event handling for dropping in colors. 
+	// ui is a jqueryUI object of Jquery objects. 
+	// ui.draggable selects the dragged element.
+	// We take it's color value.
 	$(".color-holder").on("drop", function(event, ui){
 		var outputCSSVar = $(this).attr("data-css-var");
 		var outputCSSColor = ui.draggable.attr("data-color-value");
 		
+		// Change the color of the dragged onto box.
 		$(this).css("background-color", outputCSSColor);
+		// Change the CSS Variables dynamically.
 		$("html").get(0).style.setProperty(outputCSSVar, outputCSSColor);
 	});
 
+	// Initial Palette Generation
 	generatePaletteDOM(colors);
-
-	// Modify the generation to add classes for drag and drop.
 
 
 
