273347f2151cd5925479548dfe7f04a3b1b2ce0d
Tim LaRou <larou.tim@gmail.com>
2017-03-11 10:42:38 -0600

    update for firebase

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -24,7 +24,12 @@ $("#search-button").on("click",function() {
            pokemonName = response.name;
            pokemonPicture = response.sprites.front_default;
           // console.log(pokemonPicture);
-          createPokemon();
+          try {
+            createPokemon();
+          }
+          catch(err) {
+            console.log('error');
+          }
         });
   }
 });
@@ -32,80 +37,94 @@ $("#search-button").on("click",function() {
 
 function createPokemon() {
 
-        var pokemonHolder = $("<div>");
-    pokemonHolder.attr("class","panel panel-info pokemonBox");
+      var pokemonHolder = $("<div>");
+      pokemonHolder.attr("class","panel panel-info pokemonBox");
 
-    var pokemonSpot = $("<div>");
-    pokemonSpot.attr("class","panel-body");
+      var pokemonSpot = $("<div>");
+      pokemonSpot.attr("class","panel-body");
 
-    var panelHeader = $("<div>");
-    panelHeader.attr("class","panel-heading");
-    panelHeader.text(pokemonName);
+      var panelHeader = $("<div>");
+      panelHeader.attr("class","panel-heading");
+      panelHeader.text(pokemonName);
 
-    var rowPlace = $("<div>");
-    rowPlace.attr("class","row");
+      var rowPlace = $("<div>");
+      rowPlace.attr("class","row");
 
-    var leftCol = $("<div>");
-    var rightCol = $("<div>");
-    var midCol = $("<div>");
+      var leftCol = $("<div>");
+      var rightCol = $("<div>");
+      var midCol = $("<div>");
 
-    leftCol.attr("class", "col-sm-1");
-    midCol.attr("class", "col-sm-2");
-    rightCol.attr("class","col-sm-4");
-    leftColRow = $("<div>");
-    leftColRow.attr("class", "row");
-    midColRow = $("<div>");
-    midColRow.attr("class", "row");
+      leftCol.attr("class", "col-sm-4");
+      // midCol.attr("class", "col-sm-2");
+      rightCol.attr("class","col-sm-4");
+      leftColRow = $("<div>");
+      leftColRow.attr("class", "row");
+      midColRow = $("<div>");
+      midColRow.attr("class", "row");
 
-    var pokemonImage = $("<img>");
-    pokemonImage.attr("src", pokemonPicture);
-    pokemonImage.attr('width',"150px");
-    pokemonImage.attr('height','150px');
-    pokemonImage.attr('crossOrigin','Anonymous');
+      var pokemonImage = $("<img>");
+      pokemonImage.attr("src", pokemonPicture);
+      pokemonImage.attr('width',"150px");
+      pokemonImage.attr('height','150px');
+      pokemonImage.attr('crossOrigin','Anonymous');
+      pokemonImage.crossOrigin = "Anonymous";
 
-    img2 = document.createElement('img');
-    img2.setAttribute('src', pokemonPicture);
+      img2 = document.createElement('img');
+      img2.setAttribute('src', pokemonPicture);
 
-    img2.setAttribute('width', '200px');
-    img2.setAttribute('height', '200px');
-    img2.crossOrigin = "Anonymous";
+      img2.setAttribute('width', '200px');
+      img2.setAttribute('height', '200px');
+      img2.crossOrigin = "Anonymous";
 
-    img2.addEventListener('load', function() {
-    
-        var vibrant = new Vibrant(img2);
-        var swatches = vibrant.swatches()
-        for (var swatch in swatches) {
-            if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
-                console.log(swatch, swatches[swatch].getHex())
-                var colorBox = $("<p>");
-                var pBox = $("<p>");
-                colorBox.attr("class", "colorBox")
-                var hexCode = swatches[swatch].getHex();
-                colorBox.css("background-color", hexCode)
-                pBox.text( hexCode);
-                midColRow.append(pBox);
-                leftColRow.append(colorBox);
+      img2.onload = function() {
+      // img2.addEventListener('load', function() {
+        var colorThief = new ColorThief();
+        var domColor = colorThief.getColor(img2);
+        var palette = colorThief.getPalette(img2);
+        var domColorBox = $("<p>");
+        var domPBox = $("<p>");
+        var rbgCode = "rgb(" + domColor[0] + "," + domColor[1] + "," + domColor[2]+")" ;
 
-                // if (swatch == "Vibrant") {
-                //  pokemonSpot.css("background-color",hexCode);
-                // }
-                //  if (swatch == "LightVibrant") {
-                //  midColRow.css("color",hexCode);
-                // }
-            }
+        domColorBox.attr("class", "colorBox");
+        domColorBox.css("background-color", rbgCode  );
+        domPBox.text = rbgCode;
+        // midColRow.append(domPBox);
+        leftColRow.append(domColorBox);
+
+        for (var i = 0; i < palette.length; i ++) {
+          var colorBox = $("<p>");
+          var pBox = $("<p>");
+          rbgCode = "rgb(" + palette[i][0] + "," + palette[i][1] + "," + palette[i][2]+")";
+          // pBox.text( rbgCode);
+          midColRow.append(pBox);
+
+      //             midColRow.append(pBox);
+          colorBox.attr("class", "colorBox");
+          colorBox.css("background-color",rbgCode);
+          leftColRow.append(colorBox);
         }
-    });
 
-    rightCol.append(pokemonImage);
-    leftCol.append(leftColRow);
-    midCol.append(midColRow);
-    rowPlace.append(leftCol);
-    rowPlace.append(midCol);
-    rowPlace.append(rightCol);
-    pokemonSpot.append(rowPlace);
-    pokemonHolder.append(panelHeader);
-    pokemonHolder.append(pokemonSpot);
-    $("#imagePlace").append(pokemonHolder);
+        console.log(colorThief.getColor(img2));
+        console.log(colorThief.getPalette(img2));
 
-}
+      
+      };
+
+
+
+      
+
+      rightCol.append(pokemonImage);
+      leftCol.append(leftColRow);
+      // midCol.append(midColRow);
+      rowPlace.append(leftCol);
+      // rowPlace.append(midCol);
+      rowPlace.append(rightCol);
+      pokemonSpot.append(rowPlace);
+      pokemonHolder.append(panelHeader);
+      pokemonHolder.append(pokemonSpot);
+      $("#imagePlace").append(pokemonHolder);
+    }
+
+
 });
